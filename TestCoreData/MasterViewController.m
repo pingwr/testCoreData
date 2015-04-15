6219efd3f99a964f15d07494d769a5fab11a117f
pingwr <pingwr@pwrMac.local>
2015-04-15 21:14:42 +0800

    update

--- TestCoreData/MasterViewController.m
+++ TestCoreData/MasterViewController.m
@@ -114,17 +114,15 @@
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-        [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
-            
-        NSError *error = nil;
-        if (![context save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        }
+        
+        PTCoreDataContext* mainContext = [[AppConfigures singleton] getMainContext];
+        id object = [self.fetchedResultsController objectAtIndexPath:indexPath];
+        [mainContext performSaveWithBlock:^(NSManagedObjectContext *managedObjectContext) {
+            [managedObjectContext deleteObject:object];
+        } resultBlock:nil];
+
     }
 }
 
